4a78b9fe538bdc3cb3b34c8bfb7a7c99469e3b46
VenchasS <veniy.ru@yandex.ru>
2023-11-11 16:13:43 +0300

    new web pack config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,12 +1,71 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// Specify separate paths
+const APP_DIR = path.resolve(__dirname, "./src");
 
 module.exports = {
-  //...
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
+    mode: "development",
+    devtool: "inline-source-map",
+    entry: {
+        app: path.join(__dirname, "src", "index.tsx"),
     },
-    compress: true,
-    port: 9000,
-  },
+    target: "web",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: {
+                    loader: "ts-loader",
+                    options: {
+                        compilerOptions: {
+                            noEmit: false, // this option will solve the issue
+                        },
+                    },
+                },
+                exclude: "/node_modules/",
+            },
+            {
+                test: /\.css$/,
+                include: APP_DIR,
+                use: [
+                    {
+                        loader: "style-loader",
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 1,
+                            modules: {
+                                localIdentName:
+                                    "[name]__[local]___[hash:base64:5]",
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src", "index.html"),
+        }),
+    ],
+    devServer: {
+        client: {
+            overlay: false,
+        },
+        static: {
+            directory: path.join(__dirname, "public"),
+        },
+        compress: true,
+        port: 9000,
+        hot: true,
+    },
 };
