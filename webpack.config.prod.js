2cf1a5166577da4e5b67906e28e65d3ffc8fd299
MintBrain <maximka602@gmail.com>
2024-05-20 15:33:00 +0500

    1

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
 // Specify separate paths
 const APP_DIR = path.resolve(__dirname, "./src");
 
@@ -42,5 +43,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "src", "index.html"),
     }),
+    new Dotenv()
   ],
 };
