f648caf96c08ea593a6a8f1675c828797c525c32
MintBrain <maximka602@gmail.com>
2024-06-09 22:10:35 +0500

    new profile

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -19,7 +19,9 @@ const Profile: React.FC<ProfileProps> = ({ isOwner }) => {
 
   useEffect(() => {
     if (getCookie('user') !== COOKIE) {
+      console.log('ok')
       navigate(0)
+      console.log('done')
     }
   })
 
@@ -43,14 +45,120 @@ const Profile: React.FC<ProfileProps> = ({ isOwner }) => {
     <div
       style={{
         display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '80vh',
+        flexDirection: 'row',
       }}
     >
-      <Result
-        icon={<Avatar size={64} icon={<UserOutlined />} />}
-        title={user.fullName}
+      <div
+        style={{
+          display: 'flex',
+          height: '28vh',
+          width: '20vw',
+          marginLeft: '30px',
+          marginTop: '40px',
+          borderRadius: '15px',
+          alignItems: 'center',
+          boxShadow: '0px 1px 5px 2px rgba(186,186,186,0.76)',
+          WebkitBoxShadow: '0px 1px 5px 2px rgba(186,186,186,0.76)',
+          MozBoxShadow: '0px 1px 5px 2px rgba(186,186,186,0.76)',
+        }}
+      >
+        <Avatar size={172} icon={<UserOutlined />} />
+      </div>
+      <div
+        style={{
+          height: '50vh',
+          position: 'relative',
+          width: '45vw',
+          marginLeft: '40px',
+          marginTop: '40px',
+          borderRadius: '15px',
+          boxShadow: '0px 1px 5px 2px rgba(186,186,186,0.76)',
+          WebkitBoxShadow: '0px 1px 5px 2px rgba(186,186,186,0.76)',
+          MozBoxShadow: '0px 1px 5px 2px rgba(186,186,186,0.76)',
+        }}
+      >
+        <div style={{ display: 'flex' }}>
+          <div style={{ marginLeft: '30px', marginTop: '10px' }}>
+            <p style={{ fontSize: '20px', fontWeight: '500' }}>
+              Основная Информация
+            </p>
+            <p style={{ fontSize: '16px', fontWeight: '500', color: 'gray' }}>
+              Общая информация о пользователе
+            </p>
+          </div>
+          {isOwner && (
+            <Link to='/login'>
+              <Button type='primary' block onClick={deleteCookieHandler}>
+                Сменить пользователя
+              </Button>
+            </Link>
+          )}
+        </div>
+        <div
+          style={{
+            width: 'auto',
+            color: 'darkgrey',
+            display: 'flex',
+            fontSize: '12px',
+          }}
+        >
+          <div
+            style={{
+              width: '100%',
+              height: '1px',
+              backgroundColor: 'lightgrey',
+              marginTop: '7px',
+            }}
+          ></div>
+          <div
+            style={{ width: 'auto', marginLeft: '15px', marginRight: '15px' }}
+          >
+            Контактная&nbsp;информация
+          </div>
+          <div
+            style={{
+              width: '100%',
+              height: '1px',
+              backgroundColor: 'lightgrey',
+              marginTop: '7px',
+            }}
+          >
+            {' '}
+          </div>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <ul style={{ marginTop: '20px', listStyleType: 'none' }}>
+            <li style={{ lineHeight: '4' }}>Имя пользователя:</li>
+            <li style={{ lineHeight: '4' }}>Группа:</li>
+            <li style={{ lineHeight: '4' }}>Роль:</li>
+            <li style={{ lineHeight: '3' }}>Привязка к телеграмму:</li>
+          </ul>
+          <ul
+            style={{
+              marginTop: '20px',
+              marginLeft: '20px',
+              listStyleType: 'none',
+            }}
+          >
+            <li style={{ lineHeight: '4' }}>{user.fullName}</li>
+            <li style={{ lineHeight: '4' }}>Группа</li>
+            <li style={{ lineHeight: '4' }}>Роль</li>
+            <li style={{ lineHeight: '3' }}>
+              Аккаунт успешно привязан к {tgToken?.token} <br></br>
+              <a
+                href={
+                  'https://t.me/aikido_notify_test_bot?start=' +
+                    tgToken?.token || ''
+                }
+              >
+                Привязать к другому аккаунту
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {/* title={user.fullName}
         subTitle={user.email}
         extra={
           <>
@@ -73,8 +181,7 @@ const Profile: React.FC<ProfileProps> = ({ isOwner }) => {
               </a>
             </div>
           </>
-        }
-      />
+        } */}
     </div>
   )
 }
